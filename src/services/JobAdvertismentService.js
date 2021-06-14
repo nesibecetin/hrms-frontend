1e5e8f301c47026498e40b985192e5b5a17c6c5f
nesibecetin <nesibe3cetin@hotmail.com>
2021-06-14 22:15:42 +0300

    get request completed for job advertisement

--- src/services/JobAdvertismentService.js
+++ src/services/JobAdvertismentService.js
@@ -6,5 +6,21 @@ export default class JobAdvertismentService {
     getJobAdvertisement() {
         return axios.get("http://localhost:8080/api/jobadvertisements/getall");
     }
+   
+    getAllByIsActiveAndCompanyName(companyName){
+        return axios.get("http://localhost:8080/api/jobadvertisements/getAllByIsActiveAndCompanyName?companyName="+companyName);
+    }
+    
+    getAllByIsActiveAndEndDateDesc(){
+        return axios.get("http://localhost:8080/api/jobadvertisements/getAllByIsActiveAndEndDateDesc");
+    }
+
+    getbyisactive(){
+        return axios.get("http://localhost:8080/api/jobadvertisements/getbyisactive");
+    }
+    
+    closeJobAdvertisment(id){
+        return axios.get(" http://localhost:8080/api/jobadvertisements/closeJobAdvertisment?id="+id)
+    }
 }
 
